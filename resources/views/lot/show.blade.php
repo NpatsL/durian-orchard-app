1fba54f7e9dc753561a0ae45658d3410606cc7e1
carry212121 <harris.m@ku.th>
2023-11-05 22:50:40 +0700

    css material,lot

--- resources/views/lot/show.blade.php
+++ resources/views/lot/show.blade.php
@@ -1,7 +1,4 @@
-{{-- @extends('layouts.main')
-
-@section('content') --}}
-<x-app-layout>
+{{-- <x-app-layout>
 <h1 class="text-5xl">
     <li>Lot: {{$lot->id}}</li>
     <li>Date: {{$lot->date}}</li>
@@ -18,5 +15,30 @@
     </button>
     <button><a href="/lot" class="btn btn-primary">Back</a></button>
 </div>
-{{-- @endsection --}}
+</x-app-layout> --}}
+
+<x-app-layout>
+    <div class="max-w-2xl mx-auto mt-20 p-4 shadow-md rounded-lg border-t-2 border-teal-400">
+        <div class="flex justify-between pb-4">
+            <a href="/lot" class="bg-blue-400 hover:bg-blue-600 text-black font-bold py-2 px-4 rounded">Back</a>
+            <p class="font-bold text-xl">Lot Details</p>
+        </div>
+
+        <div class="flex-1">
+            <div class="flex items-center">
+                <p class="font-bold">Lot :</p>
+                <p class="ml-2">{{$lot->id}}</p>
+            </div>
+            <div class="flex items-center">
+                <p class="font-bold">Date :</p>
+                <p class="ml-2">{{$lot->date}}</p>
+            </div>
+        </div>
+        <div class="flex justify-between mt-4">
+            <button>
+                <a class="bg-green-400 hover:bg-green-600 text-black font-bold py-2 px-4 rounded"
+             href="{{route('lot.edit',[ 'lot' => $lot])}}">Edit Lot</a>
+            </button>
+        </div>
+    </div>
 </x-app-layout>
--- resources/views/material/create.blade.php
+++ resources/views/material/create.blade.php
@@ -1,7 +1,4 @@
-{{-- @extends('layouts.main')
-
-@section('content') --}}
-<x-app-layout>
+{{-- <x-app-layout>
 <div class="w-full">
     <h2 class="text-center text-blue-400 font-bold text-2xl uppercase mb-10">Create New Material</h2>
     <div class="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
@@ -48,5 +45,60 @@
         </form>
     </div>
 </div>
-{{-- @endsection --}}
+</x-app-layout> --}}
+
+<x-app-layout>
+    <div class="max-w-2xl mx-auto mt-20 p-4 shadow-md rounded-lg border-t-2 border-teal-400">
+        <div class="flex justify-between pb-4">
+            <a href="/material" class="bg-red-400 hover:bg-red-600 text-black font-bold py-2 px-4 rounded">Back</a>
+            <p class="font-bold text-xl">Create New Material</p>
+        </div>
+
+        <form action="{{ route('material.store') }}" method="POST">
+            @csrf
+            <div class="mb-4">
+                <label for="name" class="block text-gray-700 text-sm font-bold mb-2">Material Name:</label>
+                <input type="text" id="name" name="name" autocomplete="off" value="{{old('name', '')}}" @error('name') border-red-400 @enderror placeholder="Put in material name" class="border border-gray-300 shadow p-3 w-full rounded mb-"></li>
+                @error('name')
+                    <div class="text-red-600 text-sm">
+                        {{$message}}
+                    </div>
+                @enderror    
+            </div>
+            <div class="mb-4">
+                <label for="date" class="block text-gray-700 text-sm font-bold mb-2">Material Quantity:</label>
+                <input type="text" id="qty" name="qty" autocomplete="off" value="{{old('qty', '')}}" @error('qty') border-red-400 @enderror placeholder="Put in material quantity" class="border border-gray-300 shadow p-3 w-full rounded mb-"></li>
+                @error('qty')
+                    <div class="text-red-600 text-sm">
+                        {{$message}}
+                    </div>
+                @enderror    
+            </div>
+            <div class="mb-4">
+                <label for="amount" class="block text-gray-700 text-sm font-bold mb-2">Material Unit:</label>
+                <input type="text" id="unit" name="unit" autocomplete="off" value="{{old('unit', '')}}" @error('unit') border-red-400 @enderror placeholder="Put in material unit" class="border border-gray-300 shadow p-3 w-full rounded mb-"></li>
+                @error('unit')
+                    <div class="text-red-600 text-sm">
+                        {{$message}}
+                    </div>
+                @enderror 
+                <li class="text-gray-700 text-sm font-bold mb-2">Lot Material:
+                        <select name="selected_option">
+                        @foreach($lots as $lot)
+                            <option value="{{ $lot->id }}">{{ $lot->date }}</option>
+                        @endforeach
+                        </select>
+                    
+                </li>
+            </div>
+            <style>
+                form li{
+                    list-style-type: none;
+                }
+            </style>
+            <div class="flex items-center justify-end mt-6">
+                <button type="submit" class="bg-green-400 hover:bg-green-600 text-black font-bold py-2 px-4 rounded">Submit</button>
+            </div>
+        </form>
+    </div>
 </x-app-layout>
